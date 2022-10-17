7a806e6a3b09239b63a4e470a9ce1331ca33a8c1
be nice to me <46555349+AlcoholicAndMoron@users.noreply.github.com>
2022-10-17 15:12:39 +0300

    Add files via upload

---
+++ pythonProject3/configure.py
@@ -0,0 +1,28 @@
+config = {
+    'name': 'playerbot',
+    'token': '5604566191:AAEBqPWDD1ULvX9dIKDkbK4yZuBFXe6f_nE'
+}
+list_games = {
+    '1.Counter-Strike: Global Offensive': '730',
+    '2.Dota 2': '570',
+    '3.PUBG: BATTLEGROUNDS': '578080',
+    '4.Apex Legends': '1172470',
+    '5.Lost Ark': '1599340',
+    '6.Grand Theft Auto V': '271590',
+    '7.Team Fortress 2': '440',
+    '8.Wallpaper Engine': '431960',
+    '9.NARAKA: BLADEPOINT': '1203220',
+    '10.Unturned': '304930'
+}
+numbers_games = {
+    '1': '1.Counter-Strike: Global Offensive',
+    '2': '2.Dota 2',
+    '3': '3.PUBG: BATTLEGROUNDS',
+    '4': '4.Apex Legends',
+    '5': '5.Lost Ark',
+    '6': '6.Grand Theft Auto V',
+    '7': '7.Team Fortress 2',
+    '8': '8.Wallpaper Engine',
+    '9': '9.NARAKA: BLADEPOINT',
+    '10': '10.Unturned'
+}
---
+++ pythonProject3/main.py
@@ -0,0 +1,35 @@
+import telebot
+import configure
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+bot = telebot.TeleBot(configure.config['token'])
+
+def players(name):
+    sus = []
+    url = f"https://steamcharts.com/app/{configure.list_games[name]}"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "lxml")
+    schet = soup.find_all(class_='num')
+    for i in schet:
+        sus.append(i.text.strip())
+    return name + '=' + sus[0]
+
+def games():
+    game_names = "\n".join(configure.list_games.keys())
+    return game_names
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,"Узнать онлайн в игре из списка:\n\n" + games())
+
+@bot.message_handler(content_types='text')
+def user_message(message):
+    if message.text in configure.numbers_games.keys():
+        bot.send_message(message.chat.id, players(configure.numbers_games[message.text]))
+    else:
+        bot.send_message(message.chat.id, "Пошёл нахуй!!!!!!!!!!!!!!!!!!")
+
+bot.polling(non_stop=True, interval=0)
+
